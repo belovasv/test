b19d99e33f4e391ace705e4f8846fce952883e3f
belovasv <belovasv@gmail.com>
2020-12-06 22:43:23 +0300

    add Jenkins test

--- src/test/java/Task_08_Jenkins.java
+++ src/test/java/Task_08_Jenkins.java
@@ -5,15 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Task_08_Jenkins {
 
     int a = 2;
-    int b =3;
+    int b = 3;
 
     @Test
     void checkSum() {
-        assertEquals(6,a*b,"Error");
+        assertEquals(6, a * b, "Error");
     }
 
     @Test
     void checkDif() {
-        assertEquals(1,b-a,"Error");
+        assertEquals(1, b - a, "Error");
     }
 }
